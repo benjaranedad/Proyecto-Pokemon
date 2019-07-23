b836551b2753e224c803610de0c7689794d2c600
benjaranedad <48252080+benjaranedad@users.noreply.github.com>
2019-07-23 12:39:44 -0400

    Update batalla - resultado

--- Pokemon/src/cl/ibm/batalla/Batalla.java
+++ Pokemon/src/cl/ibm/batalla/Batalla.java
@@ -1,8 +1,35 @@
 package cl.ibm.batalla;
 
+import cl.ibm.pokemon.Pokemon;
+
 public class Batalla {
-	
+
 	int tiempo;
 	int turnos;
+	Pokemon[] pokemon;
+
+	public int getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	public int getTurnos() {
+		return turnos;
+	}
+
+	public void setTurnos(int turnos) {
+		this.turnos = turnos;
+	}
+
+	public Pokemon[] getPokemon() {
+		return pokemon;
+	}
+
+	public void setPokemon(Pokemon[] pokemon) {
+		this.pokemon = pokemon;
+	}
 
 }
